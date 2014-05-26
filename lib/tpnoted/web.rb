7d45d67848675b2f0a91fc4e9904388ebdd76a7e
tiredpixel <tp@tiredpixel.com>
2014-05-26 02:22:48 +0100

    Add POST /notes,GET /notes/:id .

--- lib/tpnoted/web.rb
+++ lib/tpnoted/web.rb
@@ -1,9 +1,12 @@
 abort "! .env environment missing" unless ENV['LOG_LEVEL']
 
+$stdout.sync = true if ENV['LOG_SYNC'].to_i == 1
+
 require 'sinatra/base'
 require 'sinatra/json'
 
-$stdout.sync = true if ENV['LOG_SYNC'].to_i == 1
+require_relative 'note'
+
 
 module Tpnoted
   class Web < Sinatra::Base
@@ -26,6 +29,33 @@ module Tpnoted
         :time    => Time.now.to_i,
         :msg     => "Hello. Welcome to the tpnoted service."
       })
+    end
+    
+    post '/notes' do
+      note = Tpnoted::Note.new(params['password'],
+        :title => params['title'],
+        :body  => params['body']
+      )
+      
+      status 201
+      
+      headers 'Location' => "/notes/#{note.uuid}"
+    end
+    
+    get '/notes/:id' do
+      note = Tpnoted::Note.new(params['password'], :uuid => params['id'])
+      
+      unless note.empty?
+        status 200
+        
+        json({
+          :id    => note.uuid,
+          :title => note.title,
+          :body  => note.body,
+        })
+      else
+        status 404
+      end
     end
     
   end
--- test/tpnoted/test/web_test.rb
+++ test/tpnoted/test/web_test.rb
@@ -19,6 +19,39 @@ module Tpnoted
         refute_nil body['msg']
       end
       
+      def test_notes_post_get
+        data = {
+          'password' => "password123",
+          'title'    => "TO AUTUMN.",
+          'body'     => "Sometimes whoever seeks abroad may find" + $/ +
+                        "Thee sitting careless on a granary floor, " + $/
+        }
+        
+        post '/notes', data
+        
+        re_notes_id = /^\/notes\/([\w-]+)$/
+        
+        assert_equal 201, last_response.status
+        assert_match re_notes_id, last_response.location
+        
+        location = last_response.location
+        
+        get location, 'password' => data['password']
+        
+        body = JSON.parse(last_response.body)
+        
+        assert_equal 200, last_response.status
+        assert_equal re_notes_id.match(location)[1], body['id']
+        assert_equal data['title'], body['title']
+        assert_equal data['body'], body['body']
+      end
+      
+      def test_notes_get_not_found
+        get '/notes/X', 'password' => 'passwordX'
+        
+        assert_equal 404, last_response.status
+      end
+      
     end
   end
 end
